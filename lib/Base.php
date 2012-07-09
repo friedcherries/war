c8a008635d1490e7e7a2aa1b3865b5b16231f880
Michael Culey <mculey@amuniversal.com>
2012-07-09 14:11:51 -0500

    Removed test file and removed namespace slashes to allow for php 5.2 functionality

--- lib/Base.php
+++ lib/Base.php
@@ -5,18 +5,18 @@ class Base {
 	protected $reflection;
 
 	public function __construct() {
-		$this->reflection = new \ReflectionObject($this);
+		$this->reflection = new ReflectionObject($this);
 	}
 
 	public function __wakeup() {
-		$this->reflection = new \ReflectionObject($this);
+		$this->reflection = new ReflectionObject($this);
 	}
 
 	public function __get($var) {
 		if ($this->hasProperty($var)) {
 			return $this->$var;
 		} else {
-			throw new \Exception('Property ' . $var . ' does not exist');
+			throw new Exception('Property ' . $var . ' does not exist');
 		}
 	}
 
@@ -24,7 +24,7 @@ class Base {
 		if ($this->hasProperty($var)) {
 			$this->$var = $value;
 		} else {
-			throw new \Exception('Property ' . $var . ' does not exist.');
+			throw new Exception('Property ' . $var . ' does not exist.');
 		}
 	}
 
